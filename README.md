301cc84f8deb802a0fff1406032d7e52c25e1c3c
peacekeeper <markus.sabadello@gmail.com>
2013-10-30 17:11:42 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-sdk-csp
-=======
+<img src="http://respectnetwork.github.com/rn-member-graph-service/images/logo.png"><br>
 
-SDK for CSPs to register Cloud Names, Cloud Numbers, and Clouds
+Respect Network SDK for CSPs
+============================
+
+This is an SDK for CSPs to register Cloud Names, Cloud Numbers, and Clouds.
+
+### How to build
+
+First, you need to build the main [XDI2](http://github.com/projectdanube/xdi2) project.
+
+After that, just run
+
+    mvn clean install
+
+To build all components.
+
+### Information
+
+TODO
---
+++ src/main/java/net/respectnetwork/sdk/csp/example/ExampleChangeSecretToken.java
@@ -0,0 +1,58 @@
+package net.respectnetwork.sdk.csp.example;
+
+import net.respectnetwork.sdk.csp.CSP;
+import net.respectnetwork.sdk.csp.CloudNameRegistration;
+import xdi2.client.exceptions.Xdi2ClientException;
+import xdi2.core.xri3.XDI3Segment;
+
+public class ExampleChangeSecretToken {
+
+	/* CHOOSE THE INDIVIDUAL's CLOUD NAME HERE */
+	private static XDI3Segment cloudName = XDI3Segment.create("=dev.test.525");
+
+	/* CHOOSE THE INDIVIDUAL's SECRET TOKEN HERE */
+	private static String secretToken = "mynewsecret";
+
+	public static void main(String[] args) throws Xdi2ClientException {
+
+		CloudNameRegistration cloudNameRegistration;
+
+		// step 0: Set up CSP object
+
+		CSP csp = makeCSPNeustar();
+		//CSP csp = makeCSPTTCC();
+
+		// step 1: Look for registered Cloud Name
+
+		cloudNameRegistration = csp.checkCloudNameAvailable(cloudName);
+		if (cloudNameRegistration == null) throw new RuntimeException("Cloud Name " + cloudName + " could not be registered.");
+
+		// step 2: Change Secret Token
+
+		csp.generateCloudSecretToken(cloudNameRegistration, secretToken);
+
+		// done
+
+		System.out.println("Done changing secret token for Cloud Name " + cloudName);
+	}
+
+	static CSP makeCSPNeustar() {
+
+		XDI3Segment cspCloudNumber = XDI3Segment.create("[@]!:uuid:0baea650-823b-2475-0bae-a650823b2475");
+		String cspSecretToken = "s3cr3t";
+		String hostingEnvironmentRegistryXdiEndpoint = "http://mycloud.neustar.biz:14440/registry";
+		String hostingEnvironmentCloudBaseXdiEndpoint = "http://mycloud.neustar.biz:14440/users/";
+
+		return new CSP(cspCloudNumber, cspSecretToken, hostingEnvironmentRegistryXdiEndpoint, hostingEnvironmentCloudBaseXdiEndpoint);
+	}
+
+	static CSP makeCSPTTCC() {
+
+		XDI3Segment cspCloudNumber = XDI3Segment.create("[@]!:uuid:f34559e4-6b2b-d962-f345-59e46b2bd962");
+		String cspSecretToken = "ofniruoynwo";
+		String hostingEnvironmentRegistryXdiEndpoint = "http://clouds.ownyourinfo.com:14440/ownyourinfo-registry";
+		String hostingEnvironmentCloudBaseXdiEndpoint = "http://clouds.ownyourinfo.com:14440/ownyourinfo-users/";
+
+		return new CSP(cspCloudNumber, cspSecretToken, hostingEnvironmentRegistryXdiEndpoint, hostingEnvironmentCloudBaseXdiEndpoint);
+	}
+}
