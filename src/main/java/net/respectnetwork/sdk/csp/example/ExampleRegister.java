deadd1bc6d7ddb3858d5f91540c4e7aa40db4ce5
peacekeeper <markus.sabadello@gmail.com>
2013-11-27 18:52:22 +0100

    better exception if cloud number is wrong

--- src/main/java/net/respectnetwork/sdk/csp/example/ExampleRegister.java
+++ src/main/java/net/respectnetwork/sdk/csp/example/ExampleRegister.java
@@ -14,7 +14,7 @@ public class ExampleRegister {
 	private static CloudNumber cloudNumber = CloudNumber.createRandom(XDIConstants.CS_EQUALS);
 
 	/* CHOOSE THE INDIVIDUAL's CLOUD NAME HERE */
-	private static CloudName cloudName = CloudName.create("=dev.test.525");
+	private static CloudName cloudName = CloudName.create("=dev.test.2");
 
 	/* CHOOSE THE INDIVIDUAL's SECRET TOKEN HERE */
 	private static String secretToken = "mysecret";
