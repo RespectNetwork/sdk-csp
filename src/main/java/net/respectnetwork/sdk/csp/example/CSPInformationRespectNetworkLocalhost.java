5019f2fcfbde1747dc55a6fd5d9e28b5838ad451
peacekeeper <markus.sabadello@gmail.com>
2014-01-21 14:59:31 +0300

    adding CSP with localhost registration service for debugging purposes

---
+++ src/main/java/net/respectnetwork/sdk/csp/example/CSPInformationRespectNetworkLocalhost.java
@@ -0,0 +1,23 @@
+package net.respectnetwork.sdk.csp.example;
+
+import net.respectnetwork.sdk.csp.BasicCSPInformation;
+import xdi2.core.xri3.CloudNumber;
+import xdi2.core.xri3.XDI3Segment;
+
+public class CSPInformationRespectNetworkLocalhost extends BasicCSPInformation {
+
+	private static final long serialVersionUID = -1186435920408698291L;
+
+	public CSPInformationRespectNetworkLocalhost() {
+
+		super(
+				CloudNumber.create("[@]!:uuid:ca51aeb9-e09e-4305-89d7-87a944a1e1fa"),
+				"s3cr3t",
+				"http://mycloud-ote.neustar.biz:14440/registry",
+				"http://mycloud-ote.neustar.biz:14440/users/",
+				CloudNumber.create("[@]!:uuid:ca51aeb9-e09e-4305-89d7-87a944a1e1fa"),
+				"http://localhost:7073/",
+				XDI3Segment.create("[@]!:uuid:ca51aeb9-e09e-4305-89d7-87a944a1e1fa$to+registrar$from$do")
+				);
+	}
+}
