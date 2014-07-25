5f758922d407fb46d5ce5ca61d462fb66b2dc2d4
Barry Beechinor <barry@respectnetwork.net>
2014-07-25 16:02:09 -0700

    Updating CSPInformationTestCSPOTE to point to  RN Staging and NS Staging

--- src/main/java/net/respectnetwork/sdk/csp/example/CSPInformationTestCSPOTE.java
+++ src/main/java/net/respectnetwork/sdk/csp/example/CSPInformationTestCSPOTE.java
@@ -14,12 +14,12 @@ public class CSPInformationTestCSPOTE extends BasicCSPInformation {
 
 		super(
 				CloudNumber.create("[+]!:uuid:20e3d77c-5a0e-44d5-b949-85153fbb6577"),
-				"https://mycloud-ote.neustar.biz/testcsp-registry",
-				"https://mycloud-ote.neustar.biz/testcsp-users/",
+				"https://mycloud-stg.cloudnames.biz/testcsp-registry",
+				"https://mycloud-stg.cloudnames.biz/testcsp-users/",
 				"whitelabel123",
 				null,
 				CloudNumber.create("[+]!:uuid:ca51aeb9-e09e-4305-89d7-87a944a1e1fa"),
-				"https://registration-dev.respectnetwork.net/registration",
+				"https://registration-stage.respectnetwork.net/registration",
 				XDI3Segment.create("([+]!:uuid:ca51aeb9-e09e-4305-89d7-87a944a1e1fa/#registrar)$do"),
 				null,
 				XDIDiscoveryClient.NEUSTAR_OTE_DISCOVERY_CLIENT
